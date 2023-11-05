a82c436db74e4bd0dbbd307940c16184a33df16e
Mona Eid <104376815+Monaeid2001@users.noreply.github.com>
2023-11-05 22:56:56 +0200

    Update main.c

--- min-max-array/main.c
+++ min-max-array/main.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
-    printf("Hello world!\n");
+    int a[2000],i=0,n=0,min=0,max=0;
+
+    printf("Enter size of the array : ");
+    scanf("%d",&n);
+
+    printf("Enter elements in array : ");
+    for(; i<n; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    min=max=a[0];
+    for(i=1; i<n; i++)
+    {
+         if(min>a[i])
+		  min=a[i];
+		   if(max<a[i])
+		    max=a[i];
+    }
+     printf("minimum of array is : %d\n",min);
+          printf("maximum of array is : %d",max);
+
+
     return 0;
 }
